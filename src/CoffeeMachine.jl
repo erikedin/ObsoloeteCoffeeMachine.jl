e17e58c51bc575152bba6b287fe09919ed5b3ead
Erik Edin <erikedin@users.noreply.github.com>
2018-07-29 13:11:16 +0200

    update for 0.7

--- src/CoffeeMachine.jl
+++ src/CoffeeMachine.jl
@@ -13,9 +13,9 @@ Base.getindex(cup::Cup, liquid::Symbol) = cup.contents[liquid]
 Base.setindex!(cup::Cup, amount::Float64, liquid::Symbol) = cup.contents[liquid] = amount
 
 mutable struct Machine
-    cup::Nullable{Cup}
+    cup::Union{Cup,Nothing}
 
-    Machine() = new(Nullable{Cup}())
+    Machine() = new(nothing)
 end
 
 coffeewaspressed(machine::Machine) = fillcup!(machine, :coffee, 1.0)
@@ -23,9 +23,9 @@ function coffeewithmilkwaspressed(machine::Machine)
     fillcup!(machine, :coffee, 1.0)
     fillcup!(machine, :milk, 1.0)
 end
-cupisinthemachine(machine::Machine, cup::Cup) = machine.cup = Nullable(cup)
+cupisinthemachine(machine::Machine, cup::Cup) = machine.cup = cup
 
-fillcup!(machine::Machine, liquid::Symbol, amount::Float64) = get(machine.cup)[liquid] = amount
+fillcup!(machine::Machine, liquid::Symbol, amount::Float64) = machine.cup[liquid] = amount
 
 export Cup, Machine
 export coffeewaspressed, coffeewithmilkwaspressed, cupisinthemachine
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,2 @@
 using CoffeeMachine
-using Base.Test
-
-# write your own tests here
-# TODO: Run ExecutableSpecifications.runspec here, once it's available.
+using ExecutableSpecifications
